cca52866e44a6733bd0a8ad05da944aca2fab26b
nakica <nikolata_panayotov@abv.bg>
2022-04-11 10:50:10 +0300

    DAL added

---
+++ ProductTurnover/ProductTurnover.DAL/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using ProductTurnover.Infra;
+using System.Data;
+
+namespace ProductTurnover.DAL
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection = null;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _connection = conn;
+        }
+
+        public decimal ReadTaxation(int EAN)
+        {
+            var sql = $"SELECT c.VAT FROM Product p JOIN Category c ON P.Id = C.Id WHERE p.EAN = {EAN}";
+
+            var vat = _connection.QuerySingleOrDefault<decimal>(sql);
+
+            return vat;
+        }
+    }
+}

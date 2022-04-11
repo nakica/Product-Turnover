a2e41fbf5e418433de550a729f7252217d0c1757
nakica <nikolata_panayotov@abv.bg>
2022-04-11 23:48:19 +0300

    ProductTurnover.Database created

---
+++ ProductTurnover/ProductTurnover.Database/dbo/Tables/Category.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (64)  NOT NULL,
+    [VAT]  DECIMAL (3, 2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
---
+++ ProductTurnover/ProductTurnover.Database/dbo/Tables/Product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]         INT             IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (64)   NOT NULL,
+    [EAN]        NVARCHAR (13)   NOT NULL,
+    [Price]      DECIMAL (18, 2) NOT NULL,
+    [CategoryId] INT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Product_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
+);
+
